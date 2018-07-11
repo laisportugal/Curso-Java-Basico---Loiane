435480bb3fe25815c2e6bcca5f4af74919b7a6f6
Laís da Silva Portugal <lais.silv4@gmail.com>
2018-07-11 17:31:55 -0300

    Add files via upload

--- aula27/handson/Exer01.java
+++ aula27/handson/Exer01.java
@@ -1,15 +1,18 @@
 package aula27.handson;
 
-
 public class Exer01 {
 
 	public static void main(String[] args) {
 
 		Lampada lampada = new Lampada();
 		
-		lampada.ligarLampada();
-		lampada.desligarLampada();
-		
+		lampada.ligar();
+		lampada.mostrarEstado();
+		lampada.desligar();
+		lampada.mostrarEstado();
+		lampada.mudarEstado();
+		lampada.mostrarEstado();
+
 		
 	}
 
