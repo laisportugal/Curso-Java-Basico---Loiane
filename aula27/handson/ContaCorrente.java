132111256b4fcddf3524c3d7c0093102c81cb811
Laís da Silva Portugal <lais.silv4@gmail.com>
2018-07-11 17:31:28 -0300

    Add files via upload

--- aula27/handson/ContaCorrente.java
+++ aula27/handson/ContaCorrente.java
@@ -5,31 +5,35 @@ public class ContaCorrente {
 	String numero;
 	String agencia;
 	double saldo;
-	double saldoTotal;
 	double limiteEspecial;
-	boolean especial = false;
 
-	double sacar(double valor) {
-		if ((saldoTotal>= 0) && (especial = true)) {
-			saldoTotal = saldoTotal - valor;
-			System.out.println("O valor sacado foi " + valor);
+	void sacar(double valor) {
+
+		double saldoTotal = saldo + limiteEspecial;
+		if (saldoTotal < valor) {
+			System.out.println("Saldo insuficiente");
+		} else if (saldo < valor && saldoTotal > valor) {
+			double valorADeduzirDoLimite = valor - saldo;
+			saldo = 0;
+			limiteEspecial = limiteEspecial - valorADeduzirDoLimite;
 		} else {
-			System.out.println("Saldo insuficiente para saque.");
+			saldo = saldo - valor;
 		}
-		return saldo;
+		verificarSaldo();
 	}
 
-	double depositar(double valor) {
-		saldo = saldo + valor;
-		System.out.println("O valor depositado foi " + valor);
-		return saldo;
+	void verificarSaldo() {
+
+		System.out.println("O valor do saldo é " + (saldo + limiteEspecial));
 	}
 
-	double verificarSaldo(){
-		saldoTotal = saldo + limiteEspecial; 
-		System.out.println("O saldo é " + saldoTotal);
-	return saldoTotal;
+	void depositar(double valor) {
+		if (valor > 0) {
+			saldo = saldo + valor;
+			System.out.println("O valor depositado foi " + valor);
+			return;
+		}
+		verificarSaldo();
 	}
-	
-	
+
 }
