fb868497f497d51d06aa9ae2d12bdbe0bec8cb46
Laís da Silva Portugal <lais.silv4@gmail.com>
2018-07-13 17:21:52 -0300

    Add files via upload

---
+++ aula28/Pacotes.java
@@ -0,0 +1,15 @@
+package aula28;
+
+import aula24.handson.ContaCorrente;
+import aula24.handson.Exer06;
+
+public class Pacotes {
+
+	public static void main(String[] args) {
+
+		ContaCorrente conta;
+		Exer06 exer06; 
+		
+	}
+
+}
