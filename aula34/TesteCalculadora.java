112c4cd10b9336b988052435cb5d374eef4a6315
Laís da Silva Portugal <lais.silv4@gmail.com>
2018-08-08 16:26:32 -0300

    Add files via upload

---
+++ aula34/TesteCalculadora.java
@@ -0,0 +1,17 @@
+package aula34;
+
+
+public class TesteCalculadora {
+	static int resultSoma;
+	
+	public static void main(String[] args) {
+		
+		resultSoma= MinhaCalculadora.soma(1,2);
+				
+		
+	}
+	static int soma2Valores(int num1, int num2){
+		return MinhaCalculadora.soma(num1, num2);
+	}
+
+}
