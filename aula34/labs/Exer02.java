cf1ca89405d39cb6f7a3bb457dd0bf27cc59810e
Laís da Silva Portugal <lais.silv4@gmail.com>
2018-08-09 16:47:33 -0300

    Add files via upload

---
+++ aula34/labs/Exer02.java
@@ -0,0 +1,22 @@
+package aula34.labs;
+
+public class Exer02 {
+
+	public static void main(String[] args) {
+		
+		Calculadora.somar(1,2);
+		Calculadora.imprimirResultado();
+		Calculadora.subtrair(2,2);
+		Calculadora.imprimirResultado();
+		Calculadora.multiplicar(4,5);
+		Calculadora.imprimirResultado();
+		Calculadora.dividir(4,5);
+		Calculadora.imprimirResultadoReal();
+		Calculadora.potencia(5, 3);
+		Calculadora.imprimirResultadoReal();
+
+
+
+	}
+
+}
