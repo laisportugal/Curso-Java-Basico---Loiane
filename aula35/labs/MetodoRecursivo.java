adc8df5256d081fc42f29c631bf8b71a515f15e8
Laís da Silva Portugal <lais.silv4@gmail.com>
2018-08-15 21:30:32 -0300

    Update MetodoRecursivo.java

--- aula35/labs/MetodoRecursivo.java
+++ aula35/labs/MetodoRecursivo.java
@@ -5,14 +5,12 @@ public class MetodoRecursivo {
 	 * escreva um metodo recursivo e estatico que receba um numero inteiro positivo
 	 * N e calcule o somatorio dos numeros de 1 a N
 	 **/
-	public static int recursivo(int num) {
-		int resultado = 0;
-		for (int i = 1; i <= num; i++) {
-			resultado = i + (i+1);
-			
+		public static int somatorio(int num) {
+	
+		return num + somatorio(num-1) ;
 
-		}
-		return resultado;
+	}
+
 
 	}
 
