cf33c75b8cf2681069628b2c025944d4b8c21a3f
Laís da Silva Portugal <lais.silv4@gmail.com>
2018-08-17 18:23:46 -0300

    Add files via upload

---
+++ aula36/labs/Aluno.java
@@ -0,0 +1,29 @@
+package aula36.labs;
+
+public class Aluno {
+	private String nome;
+	private String matricula;
+	private int[] notas;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	public int[] getNotas() {
+		return notas;
+	}
+	public void setNotas(int[] notas) {
+		this.notas = notas;
+	}
+	
+	
+
+}
---
+++ aula36/labs/Curso.java
@@ -0,0 +1,35 @@
+package aula36.labs;
+
+public class Curso {
+
+	private String nome;
+	private String horario;
+	private Professor[] professores;
+	private Aluno[] alunos;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getHorario() {
+		return horario;
+	}
+	public void setHorario(String horario) {
+		this.horario = horario;
+	}
+	public Professor[] getProfessores() {
+		return professores;
+	}
+	public void setProfessores(Professor[] professores) {
+		this.professores = professores;
+	}
+	public Aluno[] getAlunos() {
+		return alunos;
+	}
+	public void setAlunos(Aluno[] alunos) {
+		this.alunos = alunos;
+	}
+	
+}
