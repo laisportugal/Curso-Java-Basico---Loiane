c76acff6c19d27aaa22b86a7db16bbfdef7e6a99
Laís da Silva Portugal <lais.silv4@gmail.com>
2018-08-30 12:19:16 -0300

    Update PessoaJuridica.java

--- aula43/PessoaJuridica.java
+++ aula43/PessoaJuridica.java
@@ -2,7 +2,7 @@ package com.loiane.cursojava.aula43;
 
 public class PessoaJuridica extends Contribuinte {
 
-	public double calcularImpostoPessoaJuridica(double rendaEmpresa) throws NullPointerException {
+	public double calcularImpostoPessoaJuridica(double rendaEmpresa)  {
 		if (rendaEmpresa != 0) {
 			valorImposto = rendaEmpresa * 0.1;
 		} else {
