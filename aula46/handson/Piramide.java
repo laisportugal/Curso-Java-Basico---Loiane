680db1e83b9b2a3dcdaec5a27176e5e93f78ec26
Laís da Silva Portugal <lais.silv4@gmail.com>
2018-09-11 18:10:01 -0300

    Update Piramide.java

--- aula46/handson/Piramide.java
+++ aula46/handson/Piramide.java
@@ -1,6 +1,20 @@
 package aula46.handson;
 
 public class Piramide extends Figura3D {
-	public double altura;
 	
+	public double altura;
+	public double areaBase;
+	public double areaLateral;
+	
+	@Override
+	public void calcularVolume() {
+		super.calcularVolume();
+		volume= 1/3*(areaBase*altura);
+	}
+	
+	@Override
+	public void calcularArea() {
+		area = areaBase + areaLateral;
+	}
+		
 }
