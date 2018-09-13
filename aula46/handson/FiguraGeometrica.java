749276012902416d2fa10bd19d61aed85e587923
Laís da Silva Portugal <lais.silv4@gmail.com>
2018-09-13 16:37:49 -0300

    Update FiguraGeometrica.java

--- aula46/handson/FiguraGeometrica.java
+++ aula46/handson/FiguraGeometrica.java
@@ -3,5 +3,20 @@ package aula46.handson;
 public class FiguraGeometrica {
 	public String nome;
 	public String cor;
+	
+	public String getNome() {
+		return nome;
+	}
 
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
 }
