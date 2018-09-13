468ff873fc8db66d885c723c466d476f0e0f5667
Laís da Silva Portugal <lais.silv4@gmail.com>
2018-09-13 16:36:47 -0300

    Update Figura2D.java

--- aula46/handson/Figura2D.java
+++ aula46/handson/Figura2D.java
@@ -3,8 +3,5 @@ package aula46.handson;
 public class Figura2D extends FiguraGeometrica implements DimensaoSuperficial{
 	double area;
 
-	public void calcularArea() {
-		// TODO Auto-generated method stub
-	}	
-
+	
 }
