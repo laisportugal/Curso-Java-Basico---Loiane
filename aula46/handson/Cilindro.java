196d52fec38031658e1765ae40fd43e4d2d60d79
Laís da Silva Portugal <lais.silv4@gmail.com>
2018-09-13 16:34:43 -0300

    Update Cilindro.java

--- aula46/handson/Cilindro.java
+++ aula46/handson/Cilindro.java
@@ -1,13 +1,39 @@
 package aula46.handson;
 
-public class Cilindro extends Figura3D{
-	
-	public void calularVolume() {
-		
+public class Cilindro extends Figura3D {
+	private double altura;
+	private double raio;
+
+	public double getAltura() {
+		return altura;
 	}
-	
-	public void calcularArea() {
-		
+
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+
+	public double getRaio() {
+		return raio;
+	}
+
+	public void setRaio(double raio) {
+		this.raio = raio;
+	}
+
+	@Override
+	public double calcularArea() {
+
+		double areaBase = Math.PI * Math.pow(raio, 2);
+		double areaLateral = 2 * Math.PI * raio * altura;
+		double areaTotal = (2 * areaBase) + areaLateral;
+
+		return areaTotal;
+	}
+
+	@Override
+	public double calcularVolume() {
+		return Math.pow(raio, 2) * altura * Math.PI;
 	}
 
 }
+
